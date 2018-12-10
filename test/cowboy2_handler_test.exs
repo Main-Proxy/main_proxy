463fbaa04f07d37d6eb0ce8f4543031f2707c10c
Jesse <jesse@gigalixir.com>
2018-12-09 22:18:12 -0800

    fixes all tests. still need to cover other cases.

--- test/cowboy2_handler_test.exs
+++ test/cowboy2_handler_test.exs
@@ -5,48 +5,27 @@ defmodule MasterProxy.Cowboy2HandlerTest do
 
   @connection Plug.Cowboy.Conn
 
+  defp build_req(scheme, method, host, path, headers \\ %{}) do
+    req = %{
+      path: path,
+      host: host,
+      port: 4000,
+      method: method,
+      headers: headers,
+      qs: "",
+      peer: {{0, 0, 0, 0, 0, 65535, 32512, 1}, 49020},
+      scheme: scheme,
+      version: :"HTTP/1.1",
+      cert: :undefined,
+    }
+  end
+
   test "no backend matches" do
     backends = [%{host: ~r/gigalixir.readthedocs.io/, plug: ""}]
     Application.put_env(:master_proxy, :backends, backends)
 
-    req = %{
-      bindings: %{},
-      body_length: 0,
-      cert: :undefined,
-      has_body: false,
-      headers: %{"accept" => "*/*", "host" => "localhost:4000", "user-agent" => "curl/7.47.0"},
-      host: "localhost",
-      host_info: :undefined,
-      method: "GET",
-      path: "/",
-      path_info: :undefined,
-      peer: {{0, 0, 0, 0, 0, 65535, 32512, 1}, 49020},
-      pid: pid_from_string("#PID<0.1.0>"),
-      port: 4000,
-      qs: "",
-      ref: HTTP,
-      scheme: "http",
-      sock: {{0, 0, 0, 0, 0, 65535, 32512, 1}, 4000},
-      streamid: 1,
-      version: :"HTTP/1.1"
-    }
-
-    # req = %{
-    #   scheme: :http,
-    #   path: "/",
-    #   host: "gigalixir.com",
-    #   port: 4000,
-    #   method: "GET",
-    #   headers: [],
-    #   qs: "",
-    #   peer: {"1.2.3.4", "40000"}
-    # }
-
-    # conn =
-    #   conn(:get, "/")
-    #   |> Map.put(:host, "gigalixir.com")
-
-    # %{adapter: {_conn, req}} = conn
+    # these are the required params..
+    req = build_req("http", "GET", "localhost", "/")
     {:ok, _req, {_handler, _opts}} = MasterProxy.Cowboy2Handler.init(req, {nil, nil})
 
     receive do
@@ -63,11 +42,31 @@ defmodule MasterProxy.Cowboy2HandlerTest do
   end
 
   defp matches_host?(backend_host, conn_host) do
-    opts = %{backends: [%{host: backend_host, plug: MasterProxy.Plug.Test}]}
+    # opts = %{backends: [%{host: backend_host, plug: MasterProxy.Plug.Test}]}
 
-    conn(:get, "/")
-    |> Map.put(:host, conn_host)
-    |> MasterProxy.Plug.call(MasterProxy.Plug.init(opts))
+    # conn(:get, "/")
+    # |> Map.put(:host, conn_host)
+    # |> MasterProxy.Plug.call(MasterProxy.Plug.init(opts))
+
+    backends = [%{host: backend_host, plug: MasterProxy.Plug.Test}]
+    Application.put_env(:master_proxy, :backends, backends)
+
+    # these are the required params..
+    req = build_req("http", "GET", conn_host, "/")
+    {:ok, _req, {_handler, _opts}} = MasterProxy.Cowboy2Handler.init(req, {nil, nil})
+
+    receive do
+      {ref, {status, headers, body}=response} -> response
+    after
+      1_000 -> flunk "timed out"
+    end
+
+    # safe to ignore?
+    # receive do
+    #   {:plug_conn, :sent} -> assert true
+    # after
+    #   1_000 -> flunk "timed out"
+    # end
   end
 
   defp host_generator do
@@ -80,29 +79,29 @@ defmodule MasterProxy.Cowboy2HandlerTest do
   property "all hosts match themselves" do
     # TODO: include hyphens?
     check all host <- host_generator do
-      conn = matches_host?(Regex.compile!(host), host)
-      assert conn.status == 200
+      {status, _headers, _body} = matches_host?(Regex.compile!(host), host)
+      assert status == 200
     end
   end
 
   property "all hosts match unspecified host" do
     check all host <- host_generator do
-      conn = matches_host?(nil, host)
-      assert conn.status == 200
+      {status, _headers, _body} = matches_host?(nil, host)
+      assert status == 200
     end
   end
 
   property "all hosts match subset" do
     check all host <- host_generator do
-      conn = matches_host?(Regex.compile!(String.slice(host, 1..-1)), host)
-      assert conn.status == 200
+      {status, _headers, _body} = matches_host?(Regex.compile!(String.slice(host, 1..-1)), host)
+      assert status == 200
     end
   end
 
   property "all hosts match empty string" do
     check all host <- host_generator do
-      conn = matches_host?(Regex.compile!(""), host)
-      assert conn.status == 200
+      {status, _headers, _body} = matches_host?(Regex.compile!(""), host)
+      assert status == 200
     end
   end
 
