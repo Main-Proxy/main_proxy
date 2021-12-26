684eb1bd35640b8518c952750ac9653298e8917c
c4710n <c4710n@users.noreply.github.com>
2021-12-26 16:18:25 +0800

    Add new option - `:server`

--- lib/master_proxy/application.ex
+++ lib/master_proxy/application.ex
@@ -7,30 +7,44 @@ defmodule MasterProxy.Application do
     import Supervisor.Spec, warn: false
 
     children =
-      Enum.reduce([:http, :https], [], fn scheme, result ->
-        case Application.get_env(:master_proxy, scheme) do
-          nil ->
-            # no config for this scheme, that's ok, just skip
-            result
+      if server?() do
+        Enum.reduce([:http, :https], [], fn scheme, result ->
+          case Application.get_env(:master_proxy, scheme) do
+            nil ->
+              # no config for this scheme, that's ok, just skip
+              result
 
-          scheme_opts ->
-            port = :proplists.get_value(:port, scheme_opts)
-            dispatch = [{:_, [{:_, MasterProxy.Cowboy2Handler, {nil, nil}}]}]
+            scheme_opts ->
+              port = :proplists.get_value(:port, scheme_opts)
+              dispatch = [{:_, [{:_, MasterProxy.Cowboy2Handler, {nil, nil}}]}]
 
-            opts =
-              [
-                port: port_to_integer(port),
-                dispatch: dispatch
-              ] ++ :proplists.delete(:port, scheme_opts)
+              opts =
+                [
+                  port: port_to_integer(port),
+                  dispatch: dispatch
+                ] ++ :proplists.delete(:port, scheme_opts)
 
-            Logger.info("[master_proxy] Listening on #{scheme} with options: #{inspect(opts)}")
+              Logger.info("[master_proxy] Listening on #{scheme} with options: #{inspect(opts)}")
 
-            [{Plug.Cowboy, scheme: scheme, plug: {nil, nil}, options: opts} | result]
-        end
-      end)
+              [{Plug.Cowboy, scheme: scheme, plug: {nil, nil}, options: opts} | result]
+          end
+        end)
+      else
+        []
+      end
 
     opts = [strategy: :one_for_one, name: MasterProxy.Supervisor]
+
     Supervisor.start_link(children, opts)
+  end
+
+  defp server?() do
+    # the server will be started in following situations:
+    # + enable `server: true` option for master_proxy (by default)
+    # + run `iex -S mix phx.server`
+    # + run `mix phx.server`
+    Application.get_env(:phoenix, :serve_endpoints, false) ||
+      Application.get_env(:master_proxy, :server, true)
   end
 
   # :undefined is what :proplist.get_value returns
